b5ec93b726446eef1620cc2051b129af17f9c05b
AAdnan-cbc <adnan.s@criskasecurity.com>
2025-05-06 08:47:49 +0530

    linear search

--- ses2/classes/searching_algorithm/LinearSearch.java
+++ ses2/classes/searching_algorithm/LinearSearch.java
@@ -2,4 +2,34 @@ package ses2.classes.searching_algorithm;
 
 public class LinearSearch {
     // Linear Search
+
+    // Linear search is an algorithm to search for a speicific element in a data
+    // structure.
+    // Search for a target value inside an array using linear search.
+    public static int linearSearch(int[] arr, int target) {
+        // Checks through each element one by one
+
+        // 1. you have an array (data stucture).
+        // 2. you want to find a specific target element in that array.
+        // 3. if the target element is found. we want to return the index at that point.
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] StudentIds = { 1, 2, 22, 23, 12, 15, 3 };
+        int target = 3;
+
+        int targetElementIndex = linearSearch(StudentIds, target);
+        System.out.println("Index at the which the Target Element was Found: " + targetElementIndex);
+    }
+
 }
--- ses2/classes/searching_algorithm/readme.md
+++ ses2/classes/searching_algorithm/readme.md
@@ -6,6 +6,22 @@
 
 - Goes throguh each line
 
+```
+### Algorithm for Linear Search.
+
+- Problem: simple search to find a target value within a list by checking elements sequentially until the target is found or the list ends.
+
+1. Start from the first element (index 0) for the collection/array/dataS.
+2. Compare the current element with the target value.
+3. if the current element matches the target value, return the position (success).
+4. if not, move to the next element and repeat step 2.
+5. if you reach the end of the collection/array without finding the target, return "not found" or -1;
+
+
+- looping
+- condition
+```
+
 ### Binary Search
 
 ### Complexity
